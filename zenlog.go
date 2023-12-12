230f94fa5ffaecd7d4ce40fca71458a4a92517e0
MomboteQ <momboteq@gmail.com>
2023-12-12 18:36:26 +0100

    [v1.0.1][FIX] Wrong functions

--- zenlog.go
+++ zenlog.go
@@ -7,62 +7,62 @@ var (
 
 // Debug prints a debug log with the given message.
 func Debug(msg string) {
-	defaultLogger.Log(DebugLevel, msg)
+	defaultLogger.Debug(msg)
 }
 
 // Debugf prints a debug log with the given format message.
 func Debugf(format string, a ...interface{}) {
-	defaultLogger.Logf(DebugLevel, format, a...)
+	defaultLogger.Debugf(format, a...)
 }
 
 // Success prints a success log with the given message.
 func Success(msg string) {
-	defaultLogger.Log(SuccessLevel, msg)
+	defaultLogger.Success(msg)
 }
 
 // Successf prints a success log with the given format message.
 func Successf(format string, a ...interface{}) {
-	defaultLogger.Logf(SuccessLevel, format, a...)
+	defaultLogger.Successf(format, a...)
 }
 
 // Info prints a info log with the given message.
 func Info(msg string) {
-	defaultLogger.Log(InfoLevel, msg)
+	defaultLogger.Info(msg)
 }
 
 // Infof prints a info log with the given format message.
 func Infof(format string, a ...interface{}) {
-	defaultLogger.Logf(InfoLevel, format, a...)
+	defaultLogger.Infof(format, a...)
 }
 
 // Warn prints a warn log with the given message.
 func Warn(msg string) {
-	defaultLogger.Log(WarnLevel, msg)
+	defaultLogger.Warn(msg)
 }
 
 // Warnf prints a warn log with the given format message.
 func Warnf(format string, a ...interface{}) {
-	defaultLogger.Logf(WarnLevel, format, a...)
+	defaultLogger.Warnf(format, a...)
 }
 
 // Error prints a error log with the given message.
 func Error(msg string) {
-	defaultLogger.Log(ErrorLevel, msg)
+	defaultLogger.Error(msg)
 }
 
 // Errorf prints a error log with the given format message.
 func Errorf(format string, a ...interface{}) {
-	defaultLogger.Logf(ErrorLevel, format, a...)
+	defaultLogger.Errorf(format, a...)
 }
 
 // Fatal prints a fatal log with the given message.
 func Fatal(msg string) {
-	defaultLogger.Log(FatalLevel, msg)
+	defaultLogger.Fatal(msg)
 }
 
 // Fatalf prints a fatal log with the given format message.
 func Fatalf(format string, a ...interface{}) {
-	defaultLogger.Logf(FatalLevel, format, a...)
+	defaultLogger.Fatalf(format, a...)
 }
 
 // SetTimeFormat sets the time format of the logger
